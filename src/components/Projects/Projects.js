39f5274c62640ef9384665e2584daa258112bd7b
AMANPUSHP23 <amanpushp2001@gmail.com>
2025-05-14 18:49:35 +0530

    add project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,14 +2,15 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import bloodbankmanagement from "../../Assets/Projects/bloodbankmanagement.png";
 import JobQuest from "../../Assets/Projects/jobquest.png";
 import portfolio from "../../Assets/Projects/portfolio.png";
 import limeroad from "../../Assets/Projects/limeroad.png";
 import foodDelight from "../../Assets/Projects/foodDelight.png";
 import careerconnect from "../../Assets/Projects/careerconnect.png";
 import routinezen from "../../Assets/Projects/routinezen.png";
-
+import aiplanner from "../../Assets/Projects/aiplanner.png";
+import lifeline from "../../Assets/Projects/lifeline.png";
+import pushpsetu from "../../Assets/Projects/pushpsetu.png";
 function Projects() {
   return (
     <Container fluid className="project-section">
@@ -46,6 +47,41 @@ function Projects() {
               description="A cutting-edge, futuristic web app for tracking and managing daily routines. Built with React and a powerful tech stack, RoutineZen blends sleek design and smooth animations to deliver an immersive user experience."
               ghLink="https://github.com/AMANPUSHP23/RoutineZen.git"
               demoLink="https://routinezen.netlify.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={aiplanner}
+              isBlog={false}
+              title="aiplanner"
+              description="The AI-Planner Dashboard is a futuristic platform that merges AI automation, sleek UI, and real-time interactions to streamline social media strategy and personal planning, simulating SaaS features and OpenAI-powered capabilities."
+              ghLink="https://github.com/AMANPUSHP23/AI-planner.git"
+              demoLink="https://ai-planner-app.netlify.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={pushpsetu}
+              isBlog={false}
+              title="pushpsetu"
+              description="PUSHP SETU is a futuristic House Management System that simplifies household tasks like tracking chores, managing expenses, and organizing inventory through a sleek, responsive interface designed to streamline and smarten daily living."
+              ghLink="https://github.com/AMANPUSHP23/Pushp-Setu.git"
+              demoLink="https://pushp-setu.netlify.app/"
+            />
+          </Col>
+
+
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={lifeline}
+              isBlog={false}
+              title="lifeline"
+              description="A modern web-based Blood Bank Management System designed to streamline blood donation and transfusion by connecting donors, recipients, and hospitals in real time with intuitive UI, smart search, and efficient coordination."
+              ghLink="https://github.com/AMANPUSHP23/LifeLine-Blood-System.git"
+              demoLink="https://lifeline-blood-system.netlify.app/"
             />
           </Col>
 
@@ -102,16 +138,7 @@ function Projects() {
          
 
          
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={bloodbankmanagement}
-              isBlog={false}
-              title="Blood Bank Management System"
-              description="A system to manage donor and recipient records, track blood inventory, and streamline operations. Built with Java and MySQL for secure access, real-time blood availability, and improved data accuracy."
-              ghLink="https://github.com/AMANPUSHP23/Blood-Bank-Management-System.git"
-              
-            />
-          </Col>
+    
 
           
         </Row>
